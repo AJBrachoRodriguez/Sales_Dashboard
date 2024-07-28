02ab10e606caedfb924937b8e09e023927a16d87
AJBrachoRodriguez <alexangelb@gmail.com>
2024-07-28 00:11:47 -0400

    Elimination of the df_map

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ st.sidebar.image('fashionShop.jpeg')
 
 ## dataframes
 df_final = pd.read_csv('df_final.csv')
-df_map = pd.read_csv('df_map.csv')
 df_bar = pd.read_csv('df_bar.csv')
 df_lines = pd.read_csv('df_lines.csv')
 df_pizza = pd.read_csv('df_pizza.csv')
